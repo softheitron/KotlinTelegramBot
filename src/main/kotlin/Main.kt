c2adebef2d3bd3432219880a724c9ccb3da25eb7
heiterss <softheitron@gmail.com>
2024-11-20 14:26:44 +0500

    KTB-02-working-with-files

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,16 @@
 package org.example
 
+import java.io.File
+
 fun main() {
-    println("Hello World!")
+
+    val wordsFile = File("words.txt")
+    wordsFile.writeText("hello привет")
+    wordsFile.appendText("\ndog собака\ncat кошка")
+
+    wordsFile.readLines().forEach { word ->
+        println(word)
+    }
+
+
 }
