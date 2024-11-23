e1a7e7421272d54175f98aeac8466fd9a560674e
heiterss <softheitron@gmail.com>
2024-11-23 16:06:11 +0500

    KTB-04-dictionary-parsing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,31 @@ package org.example
 
 import java.io.File
 
+data class Word(
+    val originalWord: String,
+    val translatedWord: String,
+    val correctAnswersCount: Int?
+)
+
 fun main() {
 
     val wordsFile = File("words.txt")
+    val dictionary = mutableListOf<Word>()
+    val fileLines = wordsFile.readLines()
 
-    wordsFile.forEachLine {
-        println(it)
+    fileLines.forEach { line ->
+        val splitLine = line.split("|")
+        dictionary.add(
+            Word(
+                originalWord = splitLine[0],
+                translatedWord = splitLine[1],
+                correctAnswersCount = splitLine[2].toIntOrNull() ?: 0
+            )
+        )
+    }
+
+    dictionary.forEach {
+        println("${it.originalWord} - ${it.translatedWord}|count: ${it.correctAnswersCount}")
     }
 
 }
