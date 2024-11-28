f51c47a081864c97fdb3c0383411714613ff02e6
heiterss <softheitron@gmail.com>
2024-11-28 23:32:46 +0500

    KTB-07-minor-fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 package org.example
+
 import java.io.File
 
 const val PERCENTAGE_HUNDRED = 100
@@ -11,61 +12,54 @@ data class Word(
 )
 
 fun main() {
-
     val dictionary = loadDictionary()
-
     while (true) {
-
         print(
             "1 - Учить слова\n" +
                     "2 - Статистика\n" +
                     "0 - Выход\n" +
                     "Ввод: "
         )
-
         val choseInput = readln()
-        when(choseInput) {
+        when (choseInput) {
             "1" -> {
-                val notLearnedWords = dictionary.filter { it.correctAnswersCount < MAX_CORRECT_ANSWERS }
-                if (notLearnedWords.isNotEmpty()){
-                    while(true) {
+                while (true) {
+                    val notLearnedWords = dictionary.filter { it.correctAnswersCount < MAX_CORRECT_ANSWERS }
+                    if (notLearnedWords.isNotEmpty()){
                         val questionWords = notLearnedWords.shuffled().take(4)
                         val correctAnswer = questionWords.random()
+                        println("${correctAnswer.originalWord}:")
                         println("\n${correctAnswer.originalWord}:")
                         questionWords.forEachIndexed { index, word ->
                             println("${index + 1} - ${word.translatedWord}")
                         }
                         print("Ввод: ")
                         val guessInput = readln().toIntOrNull()
+                    } else {
+                        println("\nВсе слова выучены!\n")
+                        break
                     }
-                } else {
-                    println("\nВсе слова выучены\n")
-                    continue
                 }
+            }
 
-            }
-            "2" -> {
-                val wordsAmount = dictionary.size
-                val learnedWords = dictionary.filter { it.correctAnswersCount >= MAX_CORRECT_ANSWERS }.size
-                val learnedPercent = (learnedWords.toDouble() / wordsAmount * PERCENTAGE_HUNDRED).toInt()
-                println(
-                    "Общее количество слов в словаре | $wordsAmount\n" +
-                    "Выучено $learnedWords из $wordsAmount слов | $learnedPercent%\n"
-                )
-            }
-            "0" -> return
-            else -> println("Неверный выбор. Введите число 1, 2 или 0")
+        "2" -> {
+            val wordsAmount = dictionary.size
+            val learnedWords = dictionary.filter { it.correctAnswersCount >= MAX_CORRECT_ANSWERS }.size
+            val learnedPercent = (learnedWords.toDouble() / wordsAmount * PERCENTAGE_HUNDRED).toInt()
+            println(
+                "\nОбщее количество слов в словаре | $wordsAmount\n" +
+                        "Выучено $learnedWords из $wordsAmount слов | $learnedPercent%\n"
+            )
         }
-
+        "0" -> return
+        else -> println("Неверный выбор. Введите число 1, 2 или 0")
     }
-
 }
-
+}
 fun loadDictionary(): List<Word> {
     val wordsFile = File("words.txt")
     val dictionary = mutableListOf<Word>()
     val fileLines = wordsFile.readLines()
-
     fileLines.forEach { line ->
         val splitLine = line.split("|")
         dictionary.add(
