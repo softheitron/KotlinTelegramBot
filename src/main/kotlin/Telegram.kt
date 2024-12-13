214eecc9009c4efc232057b6d644bcab92215d16
heiterss <softheitron@gmail.com>
2024-12-13 08:48:56 +0500

    KTB-16-statistic-work

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,16 +1,19 @@
 package org.example
 
 const val WELCOME_MESSAGE = "Hello!"
+const val START_TEXT = "/start"
 
 fun main(args: Array<String>) {
 
     val botToken = args[0]
     var updateId = 0
     val telegramBotService = TelegramBotService(botToken)
+    val trainer = LearnWordsTrainer()
 
     val messageTextRegex = "\"text\":\"(.+?)\"".toRegex()
     val idTextRegex = "\"update_id\":(\\d+)".toRegex()
-    val chatIdRegex: Regex = "\"chat\":.\"id\":(\\d+),".toRegex()
+    val chatIdRegex: Regex = "\"chat\":.\"id\":(\\d+)".toRegex()
+    val dataRegex: Regex = "\"data\":\"(.+?)\"".toRegex()
 
     while (true) {
         Thread.sleep(1000)
@@ -23,13 +26,23 @@ fun main(args: Array<String>) {
 
         val chatIdMatches = chatIdRegex.findAll(updates)
         val chatIdGroups = chatIdMatches.lastOrNull()?.groups
-        val chatId = chatIdGroups?.get(1)?.value?.toIntOrNull() ?: continue
+        val chatId = chatIdGroups?.get(1)?.value?.toLongOrNull() ?: continue
 
         val matchResult = messageTextRegex.findAll(updates)
         val groups = matchResult.lastOrNull()?.groups
         val receivedText = groups?.get(1)?.value ?: "No messages yet"
 
+        val dataMatches = dataRegex.find(updates)
+        val dataGroups = dataMatches?.groups
+        val data = dataGroups?.get(1)?.value
+
         if (receivedText.equals("hello", true)) telegramBotService.sendMessage(chatId, WELCOME_MESSAGE)
+        if (receivedText.equals(START_TEXT, true)) telegramBotService.sendMenu(chatId)
+        if (data.equals(STATISTICS_CLICK, true)) {
+            val statistics = trainer.getStatistics()
+            val statisticsMessageText = "Выучено ${statistics.learnedWords} из ${statistics.wordsAmount} слов | ${statistics.learnedPercent}%\n"
+            telegramBotService.sendMessage(chatId, statisticsMessageText)
+        }
     }
 
 }
--- src/main/kotlin/TelegramBotService.kt
+++ src/main/kotlin/TelegramBotService.kt
@@ -1,24 +1,65 @@
 package org.example
 
 import java.net.URI
+import java.net.URLEncoder
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.nio.charset.StandardCharsets
+
+const val STATISTICS_CLICK = "statistics_clicked"
+const val LEARN_WORDS_CLICK = "learn_words_clicked"
+const val TELEGRAM_API_URL = "https://api.telegram.org/bot"
 
 class TelegramBotService(private val botToken: String) {
 
     private val httpClient = HttpClient.newBuilder().build()
 
     fun getUpdates(updateId: Int): String {
-        val urlGetUpdate = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
+        val urlGetUpdate = "$TELEGRAM_API_URL$botToken/getUpdates?offset=$updateId"
         val getUpdatesRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdate)).build()
         val getUpdatesResponse = httpClient.send(getUpdatesRequest, HttpResponse.BodyHandlers.ofString())
         return getUpdatesResponse.body()
     }
 
-    fun sendMessage(chatId: Int, message: String) {
-        val sendMessageUrl = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$message"
+    fun sendMessage(chatId: Long, message: String) {
+        val encoded = URLEncoder.encode(
+            message,
+            StandardCharsets.UTF_8
+        )
+        val sendMessageUrl = "$TELEGRAM_API_URL$botToken/sendMessage?chat_id=$chatId&text=$encoded"
         val sendMessageRequest = HttpRequest.newBuilder().uri(URI.create(sendMessageUrl)).build()
+        httpClient.send(sendMessageRequest, HttpResponse.BodyHandlers.ofString())
+    }
+
+    fun sendMenu(chatId: Long) {
+        val sendMessageUrl = "$TELEGRAM_API_URL$botToken/sendMessage"
+        val sendMenuBody = """
+            {
+                "chat_id": $chatId,
+                "text": "Основное меню",
+                "reply_markup": {
+                    "inline_keyboard": [
+                        [
+                            {
+                                "text": "Изучить слова",
+                                "callback_data": "$LEARN_WORDS_CLICK"
+                            }
+                        ],
+                        [
+                            {
+                                "text": "Статистика",
+                                "callback_data": "$STATISTICS_CLICK"
+                            }
+                        ]
+                    ]
+                }
+            }
+        """.trimIndent()
+        val sendMessageRequest = HttpRequest.newBuilder().uri(URI.create(sendMessageUrl))
+            .header("Content-type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(sendMenuBody))
+            .build()
         httpClient.send(sendMessageRequest, HttpResponse.BodyHandlers.ofString())
     }
 
