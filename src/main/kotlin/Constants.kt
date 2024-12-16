8c4eab9a0b6c24f947e259ea472ae01b343a8f1e
heiterss <softheitron@gmail.com>
2024-12-16 15:49:14 +0500

    KTB-19-migration-to-kotlin-serialization-and-constants-transfer

---
+++ src/main/kotlin/Constants.kt
@@ -0,0 +1,18 @@
+package org.example
+
+const val TELEGRAM_API_URL = "https://api.telegram.org/bot"
+
+const val START_MENU = "/start"
+const val ALL_WORDS_LEARNED_MESSAGE = "Все слова выучены"
+const val RIGHT_ANSWER_MESSAGE = "Правильно!"
+const val LEARN_WORDS_TEXT = "Изучить слова"
+const val STATISTICS_TEXT = "Статистика"
+
+const val STATISTICS_CLICK = "statistics_clicked"
+const val LEARN_WORDS_CLICK = "learn_words_clicked"
+const val BACK_TO_MENU_CLICK = "back_to_menu_clicked"
+const val CALLBACK_DATA_ANSWER_PREFIX = "answer_"
+
+const val PERCENTAGE_HUNDRED = 100
+const val QUESTION_WORDS_AMOUNT = 4
+const val MAX_CORRECT_ANSWERS = 3
--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,61 +1,111 @@
 package org.example
 
-const val START_MENU = "/start"
-const val ALL_WORDS_LEARNED_MESSAGE = "Все слова выучены"
-const val RIGHT_ANSWER_MESSAGE = "Правильно!"
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Update(
+    @SerialName("update_id")
+    val updateId: Long,
+    @SerialName("message")
+    val message: Message? = null,
+    @SerialName("callback_query")
+    val callbackQuery: CallbackQuery? = null,
+)
+
+@Serializable
+data class Response(
+    @SerialName("result")
+    val result: List<Update>,
+)
+
+@Serializable
+data class Message(
+    @SerialName("text")
+    val text: String,
+    @SerialName("chat")
+    val chat: Chat,
+)
+
+@Serializable
+data class CallbackQuery(
+    @SerialName("data")
+    val data: String,
+    @SerialName("message")
+    val message: Message,
+)
+
+@Serializable
+data class Chat(
+    @SerialName("id")
+    val id: Long,
+)
+
+@Serializable
+data class SendMessageRequest(
+    @SerialName("chat_id")
+    val chatId: Long,
+    @SerialName("text")
+    val text: String,
+    @SerialName("reply_markup")
+    val replyMarkup: ReplyMarkup? = null,
+)
+
+@Serializable
+data class ReplyMarkup(
+    @SerialName("inline_keyboard")
+    val inlineKeyboard: List<List<InlineKeyboard>>
+)
+
+@Serializable
+data class InlineKeyboard(
+    @SerialName("callback_data")
+    val callbackData: String,
+    @SerialName("text")
+    val text: String,
+)
 
 fun main(args: Array<String>) {
 
     val botToken = args[0]
-    var updateId = 0
+    var lastUpdateId = 0L
     val telegramBotService = TelegramBotService(botToken)
     val trainer = LearnWordsTrainer()
 
-    val messageTextRegex = "\"text\":\"(.+?)\"".toRegex()
-    val idTextRegex = "\"update_id\":(\\d+)".toRegex()
-    val chatIdRegex: Regex = "\"chat\":.\"id\":(-*\\d+)".toRegex()
-    val dataRegex: Regex = "\"data\":\"(.+?)\"".toRegex()
-
     while (true) {
         Thread.sleep(1000)
-        val updates = telegramBotService.getUpdates(updateId)
-        println(updates)
+        val responseString = telegramBotService.getUpdates(lastUpdateId)
+        println(responseString)
+        val response: Response = telegramBotService.json.decodeFromString(responseString)
+        val update = response.result
+        val firstUpdate = update.firstOrNull() ?: continue
+        val updateId = firstUpdate.updateId
+        lastUpdateId = updateId + 1
 
-        val idMatches = idTextRegex.findAll(updates)
-        val idGroups = idMatches.lastOrNull()?.groups
-        updateId = idGroups?.get(1)?.value?.toIntOrNull()?.plus(1) ?: continue
 
-        val chatIdMatches = chatIdRegex.findAll(updates)
-        val chatIdGroups = chatIdMatches.lastOrNull()?.groups
-        val chatId = chatIdGroups?.get(1)?.value?.toLongOrNull() ?: continue
+        val chatId = firstUpdate.message?.chat?.id ?: firstUpdate.callbackQuery?.message?.chat?.id
+        val receivedText = firstUpdate.message?.text
+        val data = firstUpdate.callbackQuery?.data
 
-        val matchResult = messageTextRegex.findAll(updates)
-        val groups = matchResult.lastOrNull()?.groups
-        val receivedText = groups?.get(1)?.value ?: "No messages yet"
-
-        val dataMatches = dataRegex.find(updates)
-        val dataGroups = dataMatches?.groups
-        val data = dataGroups?.get(1)?.value
-
-        if (receivedText.equals(START_MENU, true)) telegramBotService.sendMenu(chatId)
-
-        when {
-            data.equals(LEARN_WORDS_CLICK) -> checkNextQuestionAndSend(trainer, telegramBotService, chatId)
-            data.equals(STATISTICS_CLICK) -> showStatistics(trainer, telegramBotService, chatId)
-            data.equals(BACK_TO_MENU_CLICK) -> telegramBotService.sendMenu(chatId)
-            data?.startsWith(CALLBACK_DATA_ANSWER_PREFIX, true) == true -> {
-                val answerId = data.substringAfter(CALLBACK_DATA_ANSWER_PREFIX).toIntOrNull() ?: continue
-                val lastQuestion = trainer.getLastQuestion()
-                checkAnswerAndSendMessage(
-                    answerId,
-                    lastQuestion,
-                    trainer,
-                    telegramBotService,
-                    chatId
-                )
+        if (chatId != null) {
+            if (receivedText.equals(START_MENU, true)) telegramBotService.sendMenu(chatId)
+            when {
+                data.equals(LEARN_WORDS_CLICK) -> checkNextQuestionAndSend(trainer, telegramBotService, chatId)
+                data.equals(STATISTICS_CLICK) -> showStatistics(trainer, telegramBotService, chatId)
+                data.equals(BACK_TO_MENU_CLICK) -> telegramBotService.sendMenu(chatId)
+                data?.startsWith(CALLBACK_DATA_ANSWER_PREFIX, true) == true -> {
+                    val answerId = data.substringAfter(CALLBACK_DATA_ANSWER_PREFIX).toIntOrNull() ?: continue
+                    val lastQuestion = trainer.getLastQuestion()
+                    checkAnswerAndSendMessage(
+                        answerId,
+                        lastQuestion,
+                        trainer,
+                        telegramBotService,
+                        chatId
+                    )
+                }
             }
         }
-
     }
 
 }
