4844f0b926b6837582f9560b4410e4f6e8ea88f0
heiterss <softheitron@gmail.com>
2024-12-20 11:57:19 +0500

    Gradle-application-main-Class-set

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "2.1.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    application
 }
 
 group = "org.example"
@@ -18,6 +20,11 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(20)
+}
+
+application {
+    mainClass.set("org.example.TelegramKt")
 }
